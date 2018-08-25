110bdd52a1f63b4e131dc0801738e4cdb4a811f1
Toreilly317 <Taylor@TaylorOReilly.com>
2018-08-25 11:59:28 -0400

    fix spelling

--- Compositor.js
+++ Compositor.js
@@ -1,7 +1,8 @@
-export default class Compositer {
+export default class Compositor {
   constructor() {
     this.layers = [];
   }
+
   draw(context) {
     this.layers.forEach(layer => {
       layer(context);
