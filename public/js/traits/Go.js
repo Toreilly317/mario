c0af150cae1725e092c03216314330846e5f1804
Toreilly317 <Taylor@TaylorOReilly.com>
2018-08-26 16:14:01 -0400

    add slide animation

--- public/js/traits/Go.js
+++ public/js/traits/Go.js
@@ -7,20 +7,25 @@ export default class Go extends Trait {
     this.dir = 0;
     this.acceleration = 400;
     this.dragFactor = 1 / 5000;
-
+    this.deceleration = 300;
     this.distance = 0;
     this.heading = 1;
   }
 
   update(entity, deltaTime) {
-    if (this.dir) {
+    const absX = Math.abs(entity.vel.x);
+
+    if (this.dir !== 0) {
       entity.vel.x += this.acceleration * deltaTime * this.dir;
       this.heading = this.dir;
-      this.distance += Math.abs(entity.vel.x) * deltaTime;
+    } else if (entity.vel.x !== 0) {
+      const decel = Math.min(absX, this.deceleration * deltaTime);
+      entity.vel.x += entity.vel.x > 0 ? -decel : decel;
     } else {
       this.distance = 0;
     }
-    const drag = this.dragFactor * entity.vel.x * Math.abs(entity.vel.x);
+    const drag = this.dragFactor * entity.vel.x * absX;
     entity.vel.x -= drag;
+    this.distance += absX * deltaTime;
   }
 }
